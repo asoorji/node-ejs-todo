c29c63309752345f8a9b995e24757145564df9c7
Orji <ndubuisiaso@gmail.com>
2022-11-25 07:36:43 +0100

    Modified for heroku deployment

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var app = express();
 //set up template engine
 app.set('view engine', 'ejs');
 
+const port = process.env.PORT || 5000
+
 //middleware & static files
 app.use(express.static('./public'))
 
@@ -12,11 +14,11 @@ app.use(express.static('./public'))
 todoController(app)
 
 //listen to port
-app.listen(3000);
-console.log('You are listening to port 3000');
+app.listen(port);
+console.log('You are listening to port 5000');
 
 //STEPS
 //1. Navigate to project directory in the terminal
-//2. Start server typing "nodemon app.js" in terminal
+//2. Start server typing "nodemon index.js" in terminal
 //3. Open up mongodb on brower
 //4. Enter http://localhost:3000/todo on the brower
