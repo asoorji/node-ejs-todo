486ad45a4a8daf1c5df020feb21133e65be07487
Orji <ndubuisiaso@gmail.com>
2022-11-25 07:41:48 +0100

    Modified for heroku deployment

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -15,7 +15,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 module.exports = function(app){
     
-    app.get('/todo', function(req, res){
+    app.get('/', function(req, res){
         //get data from mongodb and pass it to the view
         Todo.find({}, function(err, data){
             if(err)  throw err;
